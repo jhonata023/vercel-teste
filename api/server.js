ace0b93d19881454ab5e78a5e62e9b7962fc9dd1
Medeiros <137403955+jhonata023@users.noreply.github.com>
2026-04-10 23:08:24 +0000

    estruturando um pouco mais para receber dados de um back futuro

--- api/server.js
+++ api/server.js
@@ -1,5 +1,8 @@
 export default function handler(req, res) {
-    let quest = {
+    let bd = {
+        user: [{id:1, name: 'Jhonata', lastName: 'Medeiros', email:'jhonata@gmail.com', password: 'asd'}],
+        progressUser: [{idUser: 1, dailyTarget: 50, actualQuests: 26}],
+        questions: [{
         id: 1,
         banca: 'CEBRASPE',
         ano: 2025,
@@ -9,15 +12,30 @@ export default function handler(req, res) {
         materia: 'Direito Constitucional',
         submateria: 'Artigo 5º',
         enunciado: `No que se refere aos direitos e garantias fundamentais previstos na Constituição Federal de 1988, julgue o item a seguir:
-                    "É plena a liberdade de associação para fins lícitos, inclusive a de caráter paramilitar, desde que haja prévia autorização estatal."`,
-        alternativaA: 'Certo',
-        alternativaB: 'Errado',
+            "É plena a liberdade de associação para fins lícitos, inclusive a de caráter paramilitar, desde que haja prévia autorização estatal."`,
+        alternativas: ['Certo', 'Errado'],
         resposta: 'alternativaB',
         explicacao: 'As de caráter paramilitar não são permitidas'
+    }]
     }
+    // let quest = {
+    //     id: 1,
+    //     banca: 'CEBRASPE',
+    //     ano: 2025,
+    //     cargo: 'Agente',
+    //     orgao: 'Polícia Federal',
+    //     nivel: 'Superior',
+    //     materia: 'Direito Constitucional',
+    //     submateria: 'Artigo 5º',
+    //     enunciado: `No que se refere aos direitos e garantias fundamentais previstos na Constituição Federal de 1988, julgue o item a seguir:
+    //         "É plena a liberdade de associação para fins lícitos, inclusive a de caráter paramilitar, desde que haja prévia autorização estatal."`,
+    //     alternativas: ['Certo', 'Errado'],
+    //     resposta: 'alternativaB',
+    //     explicacao: 'As de caráter paramilitar não são permitidas'
+    // }
 
     if (req.method === 'POST') {
-        return res.status(200).json(quest);
+        return res.status(200).json(bd);
     }
 
     return res.status(405).json({ error: 'Método não permitido' });
