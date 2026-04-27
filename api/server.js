e4d52adea46197fe7eb876a1f46397e1457f7622
Medeiros <137403955+jhonata023@users.noreply.github.com>
2026-04-27 21:42:08 +0000

    Colocando a barra vertical na página inicial e mudando função de resolver questões

--- api/server.js
+++ api/server.js
@@ -14,36 +14,25 @@ export default function handler(req, res) {
         enunciado: `No que se refere aos direitos e garantias fundamentais previstos na Constituição Federal de 1988, julgue o item a seguir:
             "É plena a liberdade de associação para fins lícitos, inclusive a de caráter paramilitar, desde que haja prévia autorização estatal."`,
         alternativas: ['Certo', 'Errado'],
-        resposta: 'alternativaB',
+        resposta: 'E',
         explicacao: 'As de caráter paramilitar não são permitidas'
     }]
     }
-    // let quest = {
-    //     id: 1,
-    //     banca: 'CEBRASPE',
-    //     ano: 2025,
-    //     cargo: 'Agente',
-    //     orgao: 'Polícia Federal',
-    //     nivel: 'Superior',
-    //     materia: 'Direito Constitucional',
-    //     submateria: 'Artigo 5º',
-    //     enunciado: `No que se refere aos direitos e garantias fundamentais previstos na Constituição Federal de 1988, julgue o item a seguir:
-    //         "É plena a liberdade de associação para fins lícitos, inclusive a de caráter paramilitar, desde que haja prévia autorização estatal."`,
-    //     alternativas: ['Certo', 'Errado'],
-    //     resposta: 'alternativaB',
-    //     explicacao: 'As de caráter paramilitar não são permitidas'
-    // }
 
     if (req.method === 'POST') {
         if (req.body.op == 'renderQuestion') {
             return res.status(200).json(bd);
         } if (req.body.op == 'resolveQuest') {
-            
-        }  if (req.body.op == 'nextQuest') {
+            const question = bd.questions.find(quest => quest.id == req.body.idQuest);
 
-        }  if (req.body.op == 'previousQuest') {
-
-        } 
+            if (question) return res.status(200).json({msg: 'Parabéns, você acertou !'})
+            else return res.status(200).json({msg: 'Você errou. Tente novamente !', correct: question.resposta})
+        } if (req.body.op == 'nextQuest') {
+            return res.status(200).json({msg: 'Conexão com servidor estabelecida.'})
+        } if (req.body.op == 'home') {
+            const user = bd.user.find(user => user.id == req.body.id);
+            res.status(200).json(user);
+        }
     }
 
     return res.status(405).json({ error: 'Método não permitido' });
