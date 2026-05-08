a244fd9cd10ad16713847aa60a7dcaab422ff9f9
Medeiros <137403955+jhonata023@users.noreply.github.com>
2026-05-08 13:41:30 +0000

    Retornando somente a questão correta agora, e não o array inteiro além de fazer o correto tratamento da div de mensagem de acerto ou erro da questão

--- api/questions/render.js
+++ api/questions/render.js
@@ -1,5 +1,24 @@
 import bd from '../../lib/bd.js';
 
 export default function handler(req, res) {
-    return res.status(200).json(bd);
+    const idUser = req.body.idUser;
+    const progress = bd.progressUser.find(p => p.idUser === Number(idUser));
+
+    if (!progress) {
+        bd.progressUser.push({
+            idUser: Number(idUser),
+            queue: [1],
+            actualQuests: 0,
+            dailyTarget: 10
+        });
+    }
+    if (progress.queue.length === 0) progress.queue.push(1);
+
+    const nextQuestionId = progress.queue[0];
+    const nextQuestion = bd.questions.find(q => q.id === nextQuestionId);
+
+    return res.status(200).json({
+        question: nextQuestion,
+        progressUser: progress
+    });
 }
--- api/questions/resolve.js
+++ api/questions/resolve.js
@@ -3,9 +3,14 @@ import bd from '../../lib/bd.js';
 export default function handler(req, res) {
     try {
         const question = bd.questions.find(quest => quest.id == Number(req.body.idQuest));
+        const idUser = req.body.idUser;
+        const progress = bd.progressUser.find(p => p.idUser === Number(idUser));
             
         if (!question) return res.status(404).json({ msg: 'Questão não encontrada!', valid: false});
         if (req.body.userAnswer === question.resposta) {
+            progress.queue.shift();
+            progress.actualQuests += 1;
+
             return res.status(200).json({ 
                 msg: 'Parabéns, você acertou!',
                 explicacao: question.explicacao,
